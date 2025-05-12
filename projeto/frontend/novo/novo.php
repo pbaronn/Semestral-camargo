359bb892c401b17a5827f9172c06822dfb433a47
Dereckkk1 <128764884+Dereckkk1@users.noreply.github.com>
2025-05-12 17:26:00 -0300

    insere form

--- projeto/frontend/novo/novo.php
+++ projeto/frontend/novo/novo.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -20,90 +21,90 @@
             <a href="#" class="nav-item" id="sair"><img src="img/sair.png" alt="Sair"> Sair</a>
         </nav>
     </header>
-
     <div class="cadastro-container">
         <h1>Novo Cadastro</h1>
         
         <div class="cadastro-form">
+        <form action="processa_cadastro.php" method="post" class="cadastro-form">
             <div class="form-group">
                 <label for="nome">Nome</label>
-                <input type="text" id="nome" class="form-input">
+                <input type="text" id="nome" name="nome" class="form-input">
             </div>
 
             <div class="form-group">
                 <label for="sobrenome">Sobrenome</label>
-                <input type="text" id="sobrenome" class="form-input">
+                <input type="text" id="sobrenome" name="sobrenome" class="form-input">
             </div>
 
             <div class="form-group">
                 <label for="dataNascimento">Data de nascimento</label>
-                <input type="text" id="dataNascimento" class="form-input">
+                <input type="text" id="dt_nascimento" name="dt_nascimento" class="form-input">
             </div>
 
             <div class="form-group">
                 <label for="cpf">CPF</label>
-                <input type="text" id="cpf" class="form-input">
+                <input type="text" id="cpf" name="cpf" class="form-input">
             </div>
 
             <div class="form-group">
                 <label for="tel1">Tel.</label>
-                <input type="text" id="tel1" class="form-input">
+                <input type="text" id="telefone" name="telefone" class="form-input">
             </div>
 
             <div class="form-group">
                 <label for="tel2">Tel. 2</label>
-                <input type="text" id="tel2" class="form-input">
+                <input type="text" id="telefone2" name="telefone2" class="form-input">
             </div>
 
             <div class="form-group">
                 <label for="endereco">Endereço</label>
-                <input type="text" id="endereco" class="form-input">
+                <input type="text" id="endereco" name="endereco" class="form-input">
             </div>
 
             <div class="form-group">
                 <label for="email">E-mail</label>
-                <input type="email" id="email" class="form-input">
+                <input type="email" id="email" name="email" class="form-input">
             </div>
 
             <div class="form-group">
                 <label for="observacoes">Observações</label>
-                <textarea id="observacoes" class="form-input textarea"></textarea>
+                <textarea id="observacoes" name="observacoes" class="form-input textarea"></textarea>
             </div>
 
             <div class="form-group checkbox-group">
                 <label>Possui alguma condição de saúde pré-existente?</label>
                 <div class="radio-options">
                     <label class="radio-label">
-                        <input type="radio" name="saude" value="nao"> Não
+                        <input type="radio" name="SN_saude_preexistente" value="nao"> Não
                     </label>
                     <label class="radio-label">
-                        <input type="radio" name="saude" value="sim"> Sim
+                        <input type="radio" name="SN_saude_preexistente" value="sim"> Sim
                     </label>
                 </div>
-                <input type="text" class="form-input hidden" id="saudeDesc">
+                <input type="text" class="form-input hidden" id="DS_saude_preexistente">
             </div>
 
             <div class="form-group checkbox-group">
                 <label>Já teve alguma lesão grave ou cirurgia?</label>
                 <div class="radio-options">
                     <label class="radio-label">
-                        <input type="radio" name="lesao" value="nao"> Não
+                        <input type="radio" name="SN_lesao_cirurgia" value="nao"> Não
                     </label>
                     <label class="radio-label">
-                        <input type="radio" name="lesao" value="sim"> Sim
+                        <input type="radio" name="SN_lesao_cirurgia" value="sim"> Sim
                     </label>
                 </div>
-                <input type="text" class="form-input hidden" id="lesaoDesc">
+                <input type="text" class="form-input hidden" id="DS_lesao_cirurgia">
             </div>
 
             <div class="form-group checkbox-group">
                 <label>Possui alguma restrição médica para atividades físicas?</label>
                 <div class="radio-options">
                     <label class="radio-label">
-                        <input type="radio" name="restricao" value="nao"> Não
+                        <input type="radio" name="SN_restricao_medica" value="nao"> Não
                     </label>
                     <label class="radio-label">
-                        <input type="radio" name="restricao" value="sim"> Sim
+                        <input type="radio" name="SN_restricao_medica" value="sim"> Sim
                     </label>
                 </div>
                 <input type="text" class="form-input hidden" id="restricaoDesc">
@@ -165,19 +166,19 @@
                 <label>Está fazendo uso de alguma medicação contínua?</label>
                 <div class="radio-options">
                     <label class="radio-label">
-                        <input type="radio" name="medicacaoContinua" value="nao"> Não
+                        <input type="radio" name="SN_uso_medicacao" value="nao"> Não
                     </label>
                     <label class="radio-label">
-                        <input type="radio" name="medicacaoContinua" value="sim"> Sim
+                        <input type="radio" name="SN_uso_medicacao" value="sim"> Sim
                     </label>
                 </div>
-                <input type="text" class="form-input hidden" id="medicacaoContinuaDesc">
+                <input type="text" class="form-input hidden" id="DS_uso_medicacao">
             </div>
-
             <div class="button-group">
                 <button class="btn-salvar" id="btnSalvar">Salvar</button>
                 <button class="btn-cancelar" id="btnCancelar">Cancelar</button>
             </div>
+        </form>
         </div>
     </div>
 
